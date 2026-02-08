abd47d367ece32578177aa79135b99b572f29953
Allan <altonallan47@gmail.com>
2026-02-08 16:49:40 +0300

    FIX them Styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,14 @@ import { ReactNode } from "react";
 
 export const metadata = {
   title: "Drumy 🥁",
-  description: "Drum machine QWERTZ layout",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-[radial-gradient(circle_at_top,_#1b2350,_var(--bg-dark))] text-white">
+        {children}
+      </body>
     </html>
   );
 }
